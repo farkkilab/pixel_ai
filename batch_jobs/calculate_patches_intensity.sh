543e69bc7b4e59eaf0bc1486e0f904466e28cd31
matias <matias.aiskovich@gmail.com>
2024-02-13 12:15:04 -0300

    Mahti job patches statistics

---
+++ batch_jobs/calculate_patches_intensity.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=create_patches
+#SBATCH --account=project_2003009
+#SBATCH --time=02:00:00
+#SBATCH --partition=medium
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+module load pytorch/2.1
+source venv/bin/activate
+srun python scripts/calculate_intensity_allpatches.py --output_path --patches_path /scratch/project_2003009/NKI_project_TMAs/patches/randomly_generated/
