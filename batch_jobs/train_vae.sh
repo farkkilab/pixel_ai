07f9d83a7a4d1e4cc7d239d4a66c776dd6ddce22
matias <matias.aiskovich@gmail.com>
2024-02-20 10:42:56 -0300

    Mahti job profile

--- batch_jobs/train_vae.sh
+++ batch_jobs/train_vae.sh
@@ -5,10 +5,12 @@
 #SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100:1,nvme:500
+
+cp /scratch/project_2003009/NKI_project_TMAs/patches $LOCAL_SCRATCH
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 module load pytorch/2.1
 source venv/bin/activate
-srun python -m cProfile -o vanilla_vae_profile scripts/train_vanilla_vae.py --patches_path /scratch/project_2003009/NKI_project_TMAs/patches/randomly_generated/
+srun python -m cProfile -o vanilla_vae_profile scripts/train_vanilla_vae.py --patches_path $LOCAL_SCRATCH/randomly_generated/
