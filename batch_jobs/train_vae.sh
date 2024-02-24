f44b846bef445738538a34e2abd519db47b7e76d
matias <matias.aiskovich@gmail.com>
2024-02-24 13:59:15 -0300

    Optimise training script

--- batch_jobs/train_vae.sh
+++ batch_jobs/train_vae.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=vae_training
 #SBATCH --account=project_2003009
-#SBATCH --time=12:00:00
+#SBATCH --time=36:00:00
 #SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
@@ -13,4 +13,4 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 module load pytorch/2.1
 source venv/bin/activate
-srun ~/.local/bin/kernprof -l scripts/train_vanilla_vae.py --patches_path $LOCAL_SCRATCH/patches/randomly_generated/
+srun python scripts/train_vanilla_vae.py --patches_path $LOCAL_SCRATCH/patches/randomly_generated/
