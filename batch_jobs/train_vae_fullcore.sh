34106e525466d972c11c064a1f1d6ed6d2adbb95
matias <matias.aiskovich@gmail.com>
2024-03-12 15:06:07 +0200

    Test mahti job with lower specs, wrong data path

--- batch_jobs/train_vae_fullcore.sh
+++ batch_jobs/train_vae_fullcore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=vaefullcore_training
 #SBATCH --account=project_2003009
-#SBATCH --time=20:00:00
+#SBATCH --time=1:00:00
 #SBATCH --partition=gpumedium
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128
-#SBATCH --gres=gpu:a100:4,nvme:1000
+#SBATCH --gres=gpu:a100:1,nvme:1000
 
 rsync -av --progress /scratch/project_2003009/NKI_project_TMAs $LOCAL_SCRATCH --exclude patches
 
@@ -14,4 +14,4 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 module load pytorch/2.1
 source venv/bin/activate
-srun python scripts/train_vanilla_vae_fullcore.py --cores_path $LOCAL_SCRATCH/
+srun python scripts/train_vanilla_vae_fullcore.py --cores_path $LOCAL_SCRATCH/NKI_project_TMAs
