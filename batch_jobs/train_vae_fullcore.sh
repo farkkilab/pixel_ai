9323be6de48151dee81e8f2faac062fe8ebe0ec4
matias <matias.aiskovich@gmail.com>
2024-03-15 09:10:28 +0200

    Bigger batch size for GPUs in Mahti

--- batch_jobs/train_vae_fullcore.sh
+++ batch_jobs/train_vae_fullcore.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=vaefullcore_training
 #SBATCH --account=project_2003009
 #SBATCH --time=24:00:00
-#SBATCH --partition=gpumedium
+#SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --gres=gpu:a100:2,nvme:600
