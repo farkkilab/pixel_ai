b995f9e073e977643aa2b9171cbf7dee77f64c52
maiskovich <matias.aiskovich@gmail.com>
2025-01-29 11:05:04 +0200

    Notebook files for MIL activations on CyCIF. README.

--- batch_jobs/gigapath_tiles_encoder.sh
+++ batch_jobs/gigapath_tiles_encoder.sh
@@ -13,7 +13,7 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load pytorch/2.3
 source venv/bin/activate
 
-#srun python scripts/gigapath_encoder.py --files_path /scratch/project_2003009/NKI_histoprep_patches/224 --hf_cache_path /scratch/project_2003009/hf_cache --tiles_embedding_path /scratch/project_2003009/gigapath_tiles_embeddings/224
+srun python scripts/gigapath_encoder.py --files_path /scratch/project_2003009/NKI_histoprep_patches/224 --hf_cache_path /scratch/project_2003009/hf_cache --tiles_embedding_path /scratch/project_2003009/gigapath_tiles_embeddings/224
 #srun python scripts/gigapath_encoder.py --files_path /scratch/project_2003009/Launonen_TMA_patches/224 --hf_cache_path /scratch/project_2003009/hf_cache --tiles_embedding_path /scratch/project_2003009/Launonen_histoprep_embeddings_gigapath/224
-srun python scripts/gigapath_encoder.py --files_path /scratch/project_2003009/NKI_wsi_MHCII_histoprep_patches --hf_cache_path /scratch/project_2003009/hf_cache --tiles_embedding_path /scratch/project_2003009/MHCII_histoprep_embeddings_gigapath
+#srun python scripts/gigapath_encoder.py --files_path /scratch/project_2003009/NKI_wsi_MHCII_histoprep_patches --hf_cache_path /scratch/project_2003009/hf_cache --tiles_embedding_path /scratch/project_2003009/MHCII_histoprep_embeddings_gigapath
 
